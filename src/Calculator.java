fb0c8c9a64b3ec4190d2cbe16c5a94169733dd54
alam <53654349h@maqueta-linux.poblenou>
2024-12-03 09:42:33 +0100

    Add modulus

--- src/Calculator.java
+++ src/Calculator.java
@@ -21,12 +21,18 @@ public class Calculator {
         }
     }
 
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.multiply(2, 5));
         System.out.println(calculator.multiply(3, 7));
         System.out.println(calculator.divide(10, 5));
         System.out.println(calculator.divide(4, 7));
+        System.out.println(calculator.modulus(13, 2));
+        System.out.println(calculator.modulus(1, 9));
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
     }
