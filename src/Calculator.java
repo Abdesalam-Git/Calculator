822ea4b23cec28526f4c977974fdbfa85d076eb3
alam <53654349h@maqueta-linux.poblenou>
2024-12-03 09:11:09 +0100

    Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,19 +3,33 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
     public int subtract(int a, int b) {
         return a - b;
     }
-    public int multiply(int a, int b) {
 
+    public int multiply(int a, int b) {
         return a * b;
     }
+
+    public int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.err.println("Error");
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
         System.out.println(calculator.multiply(2, 5));
         System.out.println(calculator.multiply(3, 7));
+        System.out.println(calculator.divide(10, 5));
+        System.out.println(calculator.divide(4, 7));
         System.out.println("Addition: " + calculator.add(10, 5));
-        System.out.println("Subtraction: " + calculator.subtract(10, 5)); }
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+    }
 
 }
